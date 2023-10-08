91c8aa48d134686c3e2003ef031042d79fc8d60d
akash-saji <akashsajixyz>
2023-10-08 22:44:02 +0530

    resume added

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import  { Nav , Details , Profile , Work } from './resume';
+
+
+function App() {
+  return (
+//     <div className="App">
+//     <Nav/>
+//    <Details />
+//    <Profile/>
+// <Work/>
+//     </div>
+<Details/>
+  );
+}
+
+export default App;
---
+++ src/resume.jsx
@@ -0,0 +1,239 @@
+
+// export function Nav(){
+//     return (
+//         <>
+// <h1 className="p-2 pt-4">Akash Saji</h1>
+// <h2 className="p-2 ">Personal Information</h2>
+// <hr  />
+// </>
+//     );
+// }
+
+export function Details(){
+    return (
+
+        <>
+<div class="wrapper">
+  <div class="intro">
+    <div class="profile">
+      <div class="photo">
+        <img src="https://lh3.googleusercontent.com/pw/AIL4fc85nJwQazWCKTjxgVaMJ5RuTauCZKzQ8pQyiWeHW0etrG5c1wYEqckGY8WBLsbCIYeHMYESrbITje8iiaPG-VLMKxNp5yvo6SOsQIw39AhTWVxldOykHT5TG5adsLywTXXmU44X9PGCG8y7_j_yNEQt=w2250-h1500-s-no" style={{width:"150px" , height:"150px"}}/>
+      </div>
+      <div class="bio">
+        <h1 class="name">Akash Saji</h1>
+        <p class="profession">Full-Stack Developer</p>
+      </div>
+    </div>
+    <div class="intro-section about">
+      <h1 class="title">about me</h1>
+      <p class="paragraph">
+      Dedicated full stack developer with a proven track record in creating innovative and efficient web applications.
+Seeking a challenging position to utilize my skills in front-end and back-end development, as well as my strong
+problem-solving abilities. Committed to delivering high-quality code and exceptional user experiences.
+      </p>
+    </div>
+    <div class="intro-section contact">
+      <h1 class="title">Contact</h1>
+      <div class="info-section">
+        <i class="fas fa-phone"></i>
+        <span>(+91)9061990236</span>
+      </div>
+      <div class="info-section">
+        <i class="fas fa-map-marker-alt"></i>
+        <span>Wayanad, Kerala</span>
+      </div>
+      <div class="info-section">
+        <i class="fas fa-paper-plane"></i>
+        <span>akashsajixyz@gmail.com</span>
+      </div>
+      <div class="info-section link">
+        <i class="fab fa-facebook"></i>
+        <a target="_blank" rel="author" href="https://www.facebook.com/profile.php?id=100006277752355">
+          <span></span>
+        </a>
+      </div>
+    </div>
+    <div class="intro-section follow">
+      <h1 class="title">Follow</h1>
+      <div class="info-section link">
+        <i class="fab fa-github"></i>
+        <a target="_blank" rel="author" href="https://github.com/Akash-Saji">
+          <span>Akash-Saji@github</span>
+        </a>
+      </div>
+      <div class="info-section link">
+        <i class="fab fa-codepen"></i>
+        <a target="_blank" rel="author" href="https://www.linkedin.com/in/akash-saji-358204266/">
+          <span>linkedin</span>
+        </a>
+      </div>
+      <div class="info-section link">
+        <i class="fab fa-medium"></i>
+        <a target="_blank" rel="author" href="https://akash-saji.github.io/betasquirrel-task/">
+          <span>Tasks</span>
+        </a>
+      </div>
+    </div>
+  </div>
+  <div class="detail">
+    <div class="detail-section edu">
+      <div class="detail-title">
+        <div class="title-icon">
+          <i class="fas fa-user-graduate"></i>
+        </div>
+        <span>Experience</span>
+      </div>
+      <div class="detail-content">
+        <div class="timeline-block">
+          <h1>
+Full-stack Developer at Betasquirrel
+</h1>
+          <p>Apprenticeship</p>
+          <time>Apr 2023 - Present</time>
+          <p>Skills</p>
+          <p>Skills: Git · Agile Methodologies · Vue.js · REST APIs · Confluence · Cascading Style Sheets (CSS) · AJAX · JSON · Figma (Software) · Postman API · GitHub · Laravel · PHP · HTML5 · Bootstrap (Framework)</p>
+        </div>
+        <div class="timeline-block" >
+          <h1>
+Project Intern at  Betasquirrel</h1>
+          <p>National Chiao Tung University, Taiwan</p>
+          <time>
+Jan 2023 - Mar 2023</time>
+         
+        </div>
+      </div>
+    </div>
+    <div class="detail-section pg-skill">
+      <div class="detail-title">
+        <div class="title-icon">
+          <i class="fas fa-laptop-code"></i>
+        </div>
+        <span>Programming skills</span>
+      </div>
+      <div class="detail-content">
+        <ul class="pg-list">
+          <li>
+            <span>HTML5</span>
+            <div class="sb-skeleton">
+              <div class="skillbar1" ></div>
+            </div>
+          </li>
+          <li>
+            <span>CSS3</span>
+            <div class="sb-skeleton">
+              <div class="skillbar1" ></div>
+            </div>
+          </li>
+          <li>
+            <span>Javascript</span>
+            <div class="sb-skeleton">
+              <div class="skillbar2" ></div>
+            </div>
+          </li>
+          <li>
+            <span>PHP</span>
+            <div class="sb-skeleton">
+              <div class="skillbar3" ></div>
+            </div>
+          </li>
+          <li>
+            <span>NodeJS</span>
+            <div class="sb-skeleton">
+              <div class="skillbar4" ></div>
+            </div>
+          </li>
+          <li>
+            <span>ReactJS</span>
+            <div class="sb-skeleton">
+              <div class="skillbar5"></div>
+            </div>
+          </li>
+          <li>
+            <span>MySQL</span>
+            <div class="sb-skeleton">
+              <div class="skillbar6" ></div>
+            </div>
+          </li>
+        </ul>
+      </div>
+
+
+      <div class="detail-section pg-skill">
+      <div class="detail-title">
+        <div class="title-icon">
+          <i class="fas fa-laptop-code"></i>
+        </div>
+        <span>Developement Tools</span>
+      </div>
+
+
+      <div class="detail-content">
+        <ul class="pg-list">
+          <li>
+            <span>GitHub</span>
+            <div class="sb-skeleton">
+              <div class="skillbar1" ></div>
+            </div>
+          </li>
+          
+          <li>
+            <span>Jira</span>
+            <div class="sb-skeleton">
+              <div class="skillbar1" ></div>
+            </div>
+          </li>
+          
+        
+           
+         
+          
+        </ul>
+      </div>
+</div>
+
+    </div>
+
+    
+    <div class="detail-section interests">
+      <div class="detail-title">
+        <div class="title-icon">
+          <i class="fas fa-heart"></i>
+        </div>
+        <span>Interests</span>
+      </div>
+      <div class="detail-content">
+        <div class="outer-frame">
+          <ul class="favor-list">
+            <li>
+              <i class="fas fa-gamepad"></i>
+              <span>Game</span>
+            </li>
+            <li>
+              <i class="fas fa-paw"></i>
+              <span>Pet</span>
+            </li>
+            <li>
+              <i class="far fa-headphones-alt"></i>
+              <span>Music</span>
+            </li>
+            <li>
+              <i class="fas fa-book-spells"></i>
+              <span>Self-learning</span>
+            </li>
+            <li>
+              <i class="fas fa-user-edit"></i>
+              <span>Blog</span>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+ </div></>
+        
+
+);
+
+
+    }
+
